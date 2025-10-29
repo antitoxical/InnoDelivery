fdbe256d4952e0a6e34c8235d2c35d4be19a42f7
Danila Asepkov <danilamail05@gmail.com>
2025-10-29 12:49:53 +0300

    fix: config.rs fix

--- order_service/src/config.rs
+++ order_service/src/config.rs
@@ -1,14 +1,9 @@
 use lazy_static::lazy_static;
 use std::env;
 
-pub struct Config {
-    pub database_url: String,
-}
-
 lazy_static! {
-    pub static ref CONFIG: Config = {
+    pub static ref DATABASE_URL: String = {
         dotenvy::dotenv().ok();
-        let database_url = env::var("DATABASE_URL_ORDER").expect("DATABASE_URL_ORDER must be set");
-        Config { database_url }
+        env::var("DATABASE_URL_ORDER").expect("DATABASE_URL_ORDER must be set")
     };
 }
--- order_service/src/db.rs
+++ order_service/src/db.rs
@@ -1,10 +1,10 @@
-use crate::config::CONFIG;
+use crate::config::DATABASE_URL;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 
 pub type DbPool = Pool<ConnectionManager<PgConnection>>;
 
 pub fn create_db_pool() -> Result<DbPool, String> {
-    let manager = ConnectionManager::<PgConnection>::new(&CONFIG.database_url);
+    let manager = ConnectionManager::<PgConnection>::new(&DATABASE_URL);
     Pool::builder().build(manager).map_err(|e| e.to_string())
 }
